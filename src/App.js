3ef0edab4cd8d36faf294c2ba23287a2d60c8aa6
Khamza <khamzadev7@gmail.com>
2022-09-27 18:19:33 +0600

    write functionality on redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
 import { useDispatch, useSelector } from "react-redux";
 import {
-  addTodoAction,
-  removeLastTodoAction,
-} from "./vanilaRedux/todosReducer";
+  addTodo,
+  decrement,
+  increment,
+  removeLastTodo,
+} from "./reduxToolkit/toolkitSlice";
 
 function App() {
   const dispatch = useDispatch();
-  const count = useSelector((state) => state.count.count);
-  const todos = useSelector((state) => state.todos.todos);
+  const count = useSelector((state) => state.toolkit.count);
+  const todos = useSelector((state) => state.toolkit.todos);
 
-  const increment = () => {
-    dispatch({ type: "INCREMENT", payload: 1 });
-  };
+  // const increment = () => {
+  //   dispatch({ type: "INCREMENT", payload: 1 });
+  // };
 
-  const decrement = () => {
-    dispatch({ type: "DECREMENT", payload: 1 });
-  };
+  // const decrement = () => {
+  //   dispatch({ type: "DECREMENT", payload: 1 });
+  // };
 
-  const addTodo = (todo) => {
-    const todos = {
-      name: todo,
-      id: Date.now().toString(),
-    };
-    dispatch(addTodoAction(todos));
-  };
+  // const addTodo = (todo) => {
+  //   const todos = {
+  //     name: todo,
+  //     id: Date.now().toString(),
+  //   };
+  //   dispatch(addTodoAction(todos));
+  // };
 
-  const removeLastTodo = () => {
-    dispatch({ type: "REMOVE_LAST_TODO", payload: 1 });
-  };
+  // const removeLastTodo = () => {
+  //   dispatch({ type: "REMOVE_LAST_TODO", payload: 1 });
+  // };
 
   return (
     <div className="App">
       <h1>Счетчик : {count}</h1>
-      <button onClick={() => increment()}>INCREMENT</button>
-      <button onClick={() => decrement()}>DECREMENT</button>
-      <button onClick={() => addTodo(prompt())}>Добавить туду</button>
-      <button onClick={() => removeLastTodo()}>Удалить туду</button>
+      <button onClick={() => dispatch(increment())}>INCREMENT</button>
+      <button onClick={() => dispatch(decrement())}>DECREMENT</button>
+      <button onClick={() => dispatch(addTodo(prompt()))}>Добавить туду</button>
+      <button onClick={() => dispatch(removeLastTodo())}>Удалить туду</button>
       <div>
         {todos.map((todo) => (
-          <p>{todo.name}</p>
+          <p>{todo}</p>
         ))}
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import {store} from './vanilaRedux/index'
+import {store} from './reduxToolkit/index'
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
